cc85d1baf23a0aff6655eb95dc8985a7da29ce8d
kimorr <30475501+kimorr@users.noreply.github.com>
2017-10-22 15:59:35 -0700

    Final w/o Slideshow JS

--- js/index.js
+++ js/index.js
@@ -1,22 +1,39 @@
 $(document).ready(function(){
 
 	var self = $(this)
-	var resourceRollover = $(".resources_rollover")
 
-	resourceRollover.mouseover(function(){
+	// HAMBURGER MENU
+
+	$(".hamburger_btn").click(function(e){
+		$(".mobile_nav").toggle();
+		e.preventDefault();	
+	})
+
+	// ROLLOVER LINK
+
+	var rollover = $(".rollover")
+
+	rollover.mouseover(function(){
 		{$(".rollover_info", this).css("display", "block");}
 		{$(".download_icon img", this).css("width", "58");}
 
 	})
 
-	resourceRollover.mouseout(function(){
+	rollover.mouseout(function(){
 		{$(".rollover_info", this).css("display", "none");}
 		{$(".download_icon img", this).css("width", "55");}
 	})
+
+	// EXPAND CTA LIST
 
 	$(".expand_list_btn").click(function(){
 		$(".selection_nav ul").toggle();
 	})
 
+	// SLIDESHOW
+
+	$(".slide_forward").click(function(){
+		
+	})
 
 })
